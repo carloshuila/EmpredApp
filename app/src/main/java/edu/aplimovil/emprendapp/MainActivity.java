74abdf0c08617036c3244128f4df612882e243c3
carloshuila <carloshuila@unicauca.edu.co>
2020-10-30 18:20:49 -0500

    listar elementos de una categoria

--- app/src/main/java/edu/aplimovil/emprendapp/MainActivity.java
+++ app/src/main/java/edu/aplimovil/emprendapp/MainActivity.java
@@ -2,7 +2,11 @@ package edu.aplimovil.emprendapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import edu.aplimovil.emprendapp.categorias.CategoriaPostreActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void vercategorias(View view) {
+        Intent intent = new Intent(this, CategoriaPostreActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/aplimovil/emprendapp/categorias/PostreActivity.java
@@ -0,0 +1,16 @@
+package edu.aplimovil.emprendapp.categorias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aplimovil.emprendapp.R;
+
+public class PostreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postre);
+    }
+}
