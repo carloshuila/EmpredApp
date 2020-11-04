9e541ef1939afff3508d8a09ac345961e440e84d
Mauricio Manzano <maumanzano@unicauca.edu.cm>
2020-11-04 14:49:23 -0500

    Se agrega barra superior, barra inferior de navegacion, se organiza el contenido en layouts y se pone titulo Postres a PostreActivity,activity list postres

--- app/src/main/java/edu/aplimovil/emprendapp/postres/PostreActivity.java
+++ app/src/main/java/edu/aplimovil/emprendapp/postres/PostreActivity.java
@@ -1,7 +1,9 @@
 package edu.aplimovil.emprendapp.postres;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -18,6 +21,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.aplimovil.emprendapp.R;
+import edu.aplimovil.emprendapp.menu.MainActivity;
+import edu.aplimovil.emprendapp.menu.PedidosActivity;
+import edu.aplimovil.emprendapp.menu.PerfilActivity;
+
+import static edu.aplimovil.emprendapp.R.id.btnBarraNav;
 
 public class PostreActivity extends AppCompatActivity {
 
@@ -28,6 +36,32 @@ public class PostreActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_postres);
+
+
+        //Barra navegacion
+        BottomNavigationView navBar = findViewById(btnBarraNav);
+
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.MainActivity:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.PedidosActivity:
+                        startActivity(new Intent(getApplicationContext(), PedidosActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.PerfilActivity:
+                        startActivity(new Intent(getApplicationContext(), PerfilActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+        //Fin barra navegacion
 
         db.collection("postres")
                 .get()
