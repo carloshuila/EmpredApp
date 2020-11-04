27c28c9b8135d48b68f286b193698f240a21f4f3
Mauricio Manzano <maumanzano@unicauca.edu.cm>
2020-11-04 15:12:49 -0500

    Se agrega barra superior, barra inferior de navegacion y su logica y un titulo a metodo de pago activity

--- app/src/main/java/edu/aplimovil/emprendapp/pago/MetodoPagoActivity.java
+++ app/src/main/java/edu/aplimovil/emprendapp/pago/MetodoPagoActivity.java
@@ -1,10 +1,17 @@
 package edu.aplimovil.emprendapp.pago;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.aplimovil.emprendapp.R;
+import edu.aplimovil.emprendapp.menu.*;
+import static edu.aplimovil.emprendapp.R.id.btnBarraNav;
 
 public class MetodoPagoActivity extends AppCompatActivity {
 
@@ -12,5 +19,28 @@ public class MetodoPagoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metodo_pago);
+
+        BottomNavigationView navBar=findViewById(btnBarraNav);
+
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.MainActivity:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.PedidosActivity:
+                        startActivity(new Intent(getApplicationContext(), PedidosActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.PerfilActivity:
+                        startActivity(new Intent(getApplicationContext(), PerfilActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
